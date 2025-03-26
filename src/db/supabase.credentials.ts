9659e8c64634fc03097a2b8e3acdefe4b4f6073d
siloksan <tutel07@gmail.com>
2025-03-26 16:11:41 +0300

    docs: add description for env file

--- src/db/supabase.credentials.ts
+++ src/db/supabase.credentials.ts
@@ -2,5 +2,6 @@ export const urlSupabase = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
 export const keySupabase = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
 
 if (!urlSupabase || !keySupabase) {
+  console.error('Do not forget to rename ._env.local to .env.local!');
   throw new Error('Missing Supabase credentials');
 }
