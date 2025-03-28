9e0dde1674b4551d1a19b97518cb09a0b073d7d0
siloksan <tutel07@gmail.com>
2025-03-28 18:06:10 +0300

    fix: ci/cd

--- src/components/rest-client/rest-client.tsx
+++ src/components/rest-client/rest-client.tsx
@@ -10,7 +10,7 @@ import {
   TextField,
   Box,
   Tabs,
-  Tab
+  Tab,
 } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { ChangeEvent, SyntheticEvent, useState } from 'react';
