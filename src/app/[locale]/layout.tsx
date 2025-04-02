5ee2bae0a735ef73c6a5fdabe1bb5eceffe39393
z-e-a <87200568+z-e-a@users.noreply.github.com>
2025-04-03 01:08:58 +0300

    feat/add footer (#31)
    * feat: add footer
    * fix: fixed position of footer

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -9,6 +9,7 @@ import { NextIntlClientProvider, hasLocale } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
 import { getMessages, setRequestLocale } from 'next-intl/server';
+import { Footer } from '@/components/footer/footer';
 
 export default async function RootLayout({
   children,
@@ -38,6 +39,7 @@ export default async function RootLayout({
         <ThemeProvider theme={theme}>
           <Header initialUserName={userName} />
           {children}
+          <Footer />
           <SnackbarContainer />
         </ThemeProvider>
       </AppRouterCacheProvider>
