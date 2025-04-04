9e7020651a9d8a1db4f5df1bb90501bb0d6ae8d1
Rimfis <dimitri.rim@yandex.ru>
2025-04-04 20:43:19 +0300

    feat/data-from-url: the hook for getting data from a url

--- src/components/rest-client/rest-client.tsx
+++ src/components/rest-client/rest-client.tsx
@@ -21,17 +21,22 @@ import { Methods } from '@/types';
 import { useRouter } from 'next/navigation';
 import { bytesToBase64 } from '@/utils/converterBase64';
 import { useTranslations } from 'next-intl';
+import useUrlData from '@/hooks/use-url-data';
 
 export function RestClient() {
+  const dataFromUrl = useUrlData();
   const tabs = ['Headers', 'Query', 'Body'];
   const router = useRouter();
-  const [method, setMethod] = useState<string>(Methods.GET);
-  const [url, setUrl] = useState('');
+  const [method, setMethod] = useState<string>(dataFromUrl.method || Methods.GET);
+  const [url, setUrl] = useState(dataFromUrl.url);
   const [tab, setTab] = useState<string>(tabs[0]);
-  const [headers, setHeaders] = useState<Field[]>([initialField]);
+  const [headers, setHeaders] = useState<Field[]>(dataFromUrl.headers || [initialField]);
   const [queries, setQueries] = useState<Field[]>([initialField]);
-  const [codeBody, setCodeBody] = useState('{}');
-  const [response, setResponse] = useState<{status: number, data: string} | null>(null);
+  const [codeBody, setCodeBody] = useState(dataFromUrl.body);
+  const [response, setResponse] = useState<{
+    status: number;
+    data: string;
+  } | null>(null);
   const translate = useTranslations('RestCards');
   const translateRestClient = useTranslations('RestClient');
   const translateBtn = useTranslations('Buttons');
@@ -41,37 +46,39 @@ export function RestClient() {
     const searchParams = new URLSearchParams();
     let nextUrl = `/${location.pathname.split('/')[1]}/rest-client/${method}/${urlBase64}`;
 
-    if(method !== Methods.GET){
+    if (method !== Methods.GET) {
       const bodyBase64 = bytesToBase64(new TextEncoder().encode(codeBody));
-      nextUrl+= `/${bodyBase64}`
+      nextUrl += `/${bodyBase64}`;
     }
-    headers.map(header => {
-      if(header.isActive){
-        searchParams.set(header.fieldKey, encodeURIComponent(header.value))
+    headers.map((header) => {
+      if (header.isActive) {
+        searchParams.set(header.fieldKey, encodeURIComponent(header.value));
       }
-    })
-    nextUrl+= `?${(searchParams.toString())}`
-    router.push(nextUrl)
-  }, [codeBody, headers, method, router, url])
+    });
+    nextUrl += `?${searchParams.toString()}`;
+    router.push(nextUrl);
+  }, [codeBody, headers, method, router, url]);
 
   const handleChangeMethod = ({ target: { value } }: SelectChangeEvent) => {
     setMethod(value);
-  }
+  };
   const handleChangeUrl = ({
     target: { value },
   }: ChangeEvent<HTMLInputElement>) => setUrl(value);
   const handleChangeTab = (event: SyntheticEvent, newValue: string) =>
     setTab(newValue);
   const handleSendButton = async () => {
-    const {pathname, search} = location;
-    const response = await fetch(`/api/${pathname.split('/').slice(3).join('/')}${search}`)
-    const data = await response.json()
+    const { pathname, search } = location;
+    const response = await fetch(
+      `/api/${pathname.split('/').slice(3).join('/')}${search}`
+    );
+    const data = await response.json();
 
     setResponse({
       status: data.status,
-      data: JSON.stringify(data.data, null, 2)
-    })
-  }
+      data: JSON.stringify(data.data, null, 2),
+    });
+  };
   return (
     <Box
       sx={{ pt: '1.5em', flex: 1, display: 'flex', flexDirection: 'column' }}
@@ -126,7 +133,9 @@ export function RestClient() {
           <CodeEditor handler={setCodeBody} value={codeBody} />
         )}
       </Box>
-      {response && <ResponseField status={response.status} value={response.data} />}
+      {response && (
+        <ResponseField status={response.status} value={response.data} />
+      )}
     </Box>
   );
 }
