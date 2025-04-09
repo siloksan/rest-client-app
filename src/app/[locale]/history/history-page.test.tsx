7f5a4a347cb5ac5a02687c0a687c9563b444a755
z-e-a <e.a.zubkov@yandex.ru>
2025-04-10 00:16:52 +0300

    feat: add tests for History page

---
+++ src/app/[locale]/history/history-page.test.tsx
@@ -0,0 +1,87 @@
+import { act, render, screen } from '@testing-library/react';
+import { describe, it, vi, expect } from 'vitest';
+import messages from '../../../i18n/dictionary/en.json';
+import { NextIntlClientProvider } from 'next-intl';
+import Page from './page';
+import { LOCAL_KEYS } from '@/constants/local-keys';
+
+describe('HistoryTableRow', () => {
+  const testHistoryRecords = [
+    {
+      user: 'test',
+      requestDate: 1,
+      requestMethod: 'GET',
+      requestedUrl: 'http://jsonplaceholder.typicode.com/todos/1',
+      innerUrl:
+        'http://localhost:3000/en/rest-client/GET/aHR0cDovL2pzb25wbGFjZWhvbGRlci50eXBpY29kZS5jb20vdG9kb3MvMQ==',
+    },
+    {
+      user: 'test',
+      requestDate: 2,
+      requestMethod: 'POST',
+      requestedUrl: 'http://jsonplaceholder.typicode.com/todos/2',
+      innerUrl:
+        'http://localhost:3000/en/rest-client/POST/aHR0cDovL2pzb25wbGFjZWhvbGRlci50eXBpY29kZS5jb20vdG9kb3MvMg==',
+    },
+  ];
+
+  vi.mock(import('next-intl'), async (importOriginal) => {
+    const actual = await importOriginal();
+    return {
+      ...actual,
+      useLocale: vi.fn(() => 'en'),
+    };
+  });
+
+  it('should render empty HistoryTable component without crash', async () => {
+    render(
+      <NextIntlClientProvider locale="en" messages={messages}>
+        <Page />
+      </NextIntlClientProvider>
+    );
+
+    expect(
+      screen.getByText(messages.HistoryPage.emptyMessage)
+    ).toBeInTheDocument();
+  });
+
+  it('should render not empty HistoryTable component without crash', async () => {
+    localStorage.setItem(
+      LOCAL_KEYS.HISTORY,
+      JSON.stringify(testHistoryRecords)
+    );
+
+    render(
+      <NextIntlClientProvider locale="en" messages={messages}>
+        <Page />
+      </NextIntlClientProvider>
+    );
+
+    expect(
+      screen.getByText(testHistoryRecords[0].requestedUrl)
+    ).toBeInTheDocument();
+
+    const deleteBtns = screen.getAllByTitle(
+      messages.HistoryPage.buttons.delete
+    );
+
+    await act(async () => {
+      deleteBtns[0].click();
+    });
+
+    expect(
+      JSON.parse(localStorage.getItem(LOCAL_KEYS.HISTORY) ?? '').length
+    ).toEqual(1);
+
+    const clearBtn = screen.getByText(messages.HistoryPage.buttons.clear);
+    expect(clearBtn).toBeInTheDocument();
+
+    await act(async () => {
+      clearBtn.click();
+    });
+
+    expect(
+      JSON.parse(localStorage.getItem(LOCAL_KEYS.HISTORY) ?? '').length
+    ).toEqual(0);
+  });
+});
