3f8c32ec53d6e416163dcc9f5a5ec969bf374f42
Rimfis <dimitri.rim@yandex.ru>
2025-04-19 15:45:40 +0300

    feat/api-tests: add tests for api route

--- src/app/api/[...slug]/route.ts
+++ src/app/api/[...slug]/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET(
   request: Request,
-  { params }: { params: Promise<{ slug: string }> }
+  { params }: { params: Promise<{ slug: string[] }> }
 ) {
   const {
     slug: [method, urlBase64, body],
